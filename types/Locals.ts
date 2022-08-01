db67ef10e1e66a826d9bfef2999bfa9634aa295d
quangsang239 <quangsang239@gmail.com>
2022-08-01 22:01:30 +0700

    first commit

---
+++ types/Locals.ts
@@ -0,0 +1,5 @@
+import Data from "./data";
+export default interface ILocal extends Data {
+  longitude: number;
+  latitude: number;
+}
---
+++ types/data.ts
@@ -0,0 +1,4 @@
+export default interface IData {
+  id: number;
+  address: string;
+}
